4249780d242de0290791c05123ba692771c73f5b
Patrick McGill <pqmcgill@gmail.com>
2016-07-18 15:23:03 -0400

    added devTool support

--- index.js
+++ index.js
@@ -3,13 +3,17 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
+import DevTools from './containers/DevTools';
 import App from './containers/App';
 
 const store = configureStore();
 
 render (
 	<Provider store={ store }>
-		<App />
+		<div>
+			<App />
+			<DevTools />
+		</div>
 	</Provider>,
 	document.getElementById('root')
 );
--- store/configureStore.js
+++ store/configureStore.js
@@ -1,8 +1,19 @@
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
+import { persistState } from 'redux-devtools';
 import rootReducer from '../reducers';
+import DevTools from '../containers/DevTools';
+
+const enhancer = compose(
+	DevTools.instrument(),
+	persistState(
+		window.location.href.match(
+			/[?&]debug_session=([^&#]+)\b/
+		)
+	)
+);
 
 export default function configureStore(preloadedState = {}) {
-	const store = createStore(rootReducer, preloadedState);
+	const store = createStore(rootReducer, preloadedState, enhancer);
 
 	if (module.hot) {
 		// Enable Webpack hot module replacement for reducers
